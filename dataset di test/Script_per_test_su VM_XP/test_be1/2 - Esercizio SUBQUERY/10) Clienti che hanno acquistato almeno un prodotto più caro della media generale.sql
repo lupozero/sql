728f721fcdec7f42748552152fe08e9e70442690
lupozero <albert0lup0@icloud.com>
2026-04-14 14:48:14 +0200

    merghe with mac os

--- dataset di test/Script_per_test_su VM_XP/test_be1/2 - Esercizio SUBQUERY/10) Clienti che hanno acquistato almeno un prodotto più caro della media generale.sql
+++
@@ -1,5 +0,0 @@
-SELECT DISTINCT id_cliente
-FROM ordini_mono o
-JOIN prodotti p ON o.id_prodotto = p.id_prodotto
-WHERE p.prezzo >
-      (SELECT AVG(prezzo) FROM prodotti);
--- dataset di test/Script_per_test_su VM_XP/test_be1/3 - Esercizio SUBQUERY CORRELATE/11) Clienti che hanno fatto ordini_mono più costosi della loro media.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT o1.id_cliente
-FROM ordini_mono o1
-WHERE o1.importo >
-      (SELECT AVG(o2.importo)
-       FROM ordini_mono o2
-       WHERE o2.id_cliente = o1.id_cliente);
